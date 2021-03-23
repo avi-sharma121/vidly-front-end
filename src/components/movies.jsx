bb18879f128fbc21a47607c3a95f0dc05f35e5d1
avi-sharma121 <kumaravinashsharma82@protonmail.com>
2021-03-23 22:54:47 +0530

    rendring static data and adding Like icon with full work v1.02

---
+++ src/components/movies.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import {getMovies} from '../services/fakeMovieService';
+import Like from './common/like';
+import Pagination from './common/pagination';
+
+class Movies extends Component {
+    state = { 
+        movies:getMovies(),
+        pageSize:4
+     }
+
+     handleDelete=(movie)=>{
+       // console.log(movie);
+       const movies=this.state.movies.filter(m=>m._id!==movie._id);
+       this.setState({movies});
+     };
+
+     handleLike=(movie)=>{
+    
+       // console.log('liked');
+       const movies =[...this.state.movies];
+       const index = movies.indexOf(movie);
+       movies[index] = {...movies[index]};
+       movies[index].liked = !movies[index].liked;
+       this.setState({movies});
+     };
+
+handlePageChange=(page)=>{
+    console.log(page);
+}
+
+    render() { 
+
+        const {length:count}=this.state.movies;
+        if(this.state.movies.length===0)
+        return <p>There are no movies in the database.</p>;
+
+        return ( 
+            <>
+            <p>Showing {count} movies in the database.</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Ttile</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.movies.map(movie=>(
+                    <tr key={movie._id}>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                        <td><Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/></td>
+                        <td><button onClick={()=>{this.handleDelete(movie)}} className="btn btn-danger btn-sm">Delete</button></td>
+                    </tr>
+                    ))}
+                   
+                </tbody>
+            </table>
+           <Pagination itemCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}/>
+            </>
+         );
+    }
+}
+ 
+export default Movies;
